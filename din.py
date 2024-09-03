d2cbd462bd257723a4d4c2e06e6faae8b3e19b38
ChakKaker <nikitavasin97@mail.ru>
2024-09-03 14:37:08 +0300

    Динамическая типизация

---
+++ din.py
@@ -0,0 +1,13 @@
+name = "Nikita"
+print(name)
+age = 27
+print(age)
+print(age + 1)
+is_student = "True"
+print(is_student)
+
+
+print('name:',name,
+      'age:',age,
+      'new age:',(age + 1),
+      'is_student:',is_student)
