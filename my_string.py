23dcefdf88ed09e782b5d281a691282fc6adf787
ChakKaker <nikitavasin97@mail.ru>
2024-09-05 13:25:28 +0300

    Организация программ и методы строк

---
+++ my_string.py
@@ -0,0 +1,11 @@
+my_string =input('python')
+
+
+print(len(my_string))
+
+#print('my_string'.upper())
+#print('my_string'.lower())
+#print('my_string'.replace('my_string', 'mystring'))
+#print('my_string'[0])
+#print('my_string'[-1])
+
